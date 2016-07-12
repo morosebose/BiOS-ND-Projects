60e2dcbc953ebeb76f41f52ceff8487418d43a11
Surajit A Bose <surajit@udacity.com>
2016-07-12 12:14:37 -0700

    Initial commit for BiOS projects

---
+++ PirateFleet1/Pirate Fleet/ControlCenter.swift
@@ -0,0 +1,66 @@
+//
+//  ControlCenter.swift
+//  Pirate Fleet
+//
+//  Created by Jarrod Parkes on 9/2/15.
+//  Copyright © 2015 Udacity. All rights reserved.
+//
+
+struct GridLocation {
+    let x: Int
+    let y: Int
+}
+
+struct Ship {
+    let length: Int
+    let location: GridLocation
+    let isVertical: Bool
+}
+
+struct Mine: _Mine_ {
+    let location: GridLocation
+    let explosionText: String
+
+}
+
+class ControlCenter {
+    
+    func addShipsAndMines(human: Human) {
+        let smallShip = Ship(length: 2, location: GridLocation(x: 2, y: 2), isVertical: true)
+        let mediumShip1 = Ship(length: 3, location: GridLocation(x: 1, y: 0), isVertical: false)
+        let mediumShip2 = Ship(length: 3, location: GridLocation(x: 5, y: 4), isVertical: true)
+        let largeShip = Ship(length: 4, location: GridLocation(x: 0, y: 6), isVertical: false)
+        let extraLargeShip = Ship(length: 5, location: GridLocation(x: 7, y: 3), isVertical: true)
+
+        let mine1 = Mine(location: GridLocation(x: 4,y: 2), explosionText: "Boom! Your opponent hit a mine!")
+        let mine2 = Mine(location: GridLocation(x: 1,y: 3), explosionText: "Blam! Your mine has been hit!")
+
+        human.addShipToGrid(smallShip)
+        human.addShipToGrid(mediumShip1)
+        human.addShipToGrid(mediumShip2)
+        human.addShipToGrid(largeShip)
+        human.addShipToGrid(extraLargeShip)
+
+        human.addMineToGrid(mine1)
+        human.addMineToGrid(mine2)
+    }
+    
+    func calculateFinalScore(gameStats: GameStats) -> Int {
+        
+        var finalScore: Int
+        let numShipsPerPlayer = 5
+
+        let enemyShipsSunk = numShipsPerPlayer - gameStats.enemyShipsRemaining
+        let humanShipsRemaining = numShipsPerPlayer - gameStats.humanShipsSunk
+        let numGuesses = gameStats.numberOfHitsOnEnemy + gameStats.numberOfMissesByHuman
+
+        print ("Enemy ships sunk: \(enemyShipsSunk)")
+        print ("Human ships remaining: \(humanShipsRemaining)")
+        print ("Number of guesses: \(numGuesses)")
+        
+        finalScore = (enemyShipsSunk * gameStats.sinkBonus) + (humanShipsRemaining * gameStats.shipBonus) - (numGuesses * gameStats.guessPenalty)
+
+        
+        return finalScore
+    }
+}
